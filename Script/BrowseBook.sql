04a6fbaa6386f4c4c24223779027045b8e4b500f
SadsaucR <cks6023159@gmail.com>
2024-12-09 16:20:34 +0800

    12/9 更新ER圖

--- Script/BrowseBook.sql
+++ Script/BrowseBook.sql
@@ -5,7 +5,16 @@ go
 create procedure BrowseBook
 --動作
 as begin
-	select bname as '書名',type as '類型',price as '價格',status as '狀態',wname as '所在倉庫',setdate as '最新一筆入庫資料' from bookinfo
+	select 
+			bookinfo.bid as '書本ID',
+			bname as '書名',
+			type as '類型',
+			price as '價格',
+			status as '狀態',
+			wname as '所在倉庫',
+			quantity as '庫存量',
+			setdate as '最新一筆入庫資料' 	
+from bookinfo
 	left join inventory
 		on bookinfo.bid=inventory.bid
 	left join warehouse
--- Script/restock.sql
+++ Script/restock.sql
@@ -3,7 +3,7 @@ go
 create procedure restock
 	@bid int,
 	@q int   --增加的數量
-as
+as                                                                                                
 begin
 	update inventory set quantity = quantity + @q where bid = @bid
 	update inventory set setdate = getdate() where bid = @bid
